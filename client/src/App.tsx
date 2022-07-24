bae6a336507d17e87100cae105e75106f0516f5a
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-24 16:47:09 +0530

    created Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <div className="container flex align-items-center justify-content-between">
+        <div className="navbar__left">
+          <h1 className='bold'>Bookmarks</h1>
+        </div>
+        <div className="navbar__right flex align-items-center">
+          <Link to={'/login'}>Login</Link>
+          <Link to={'/signup'}>
+            <Button type="primary" shape="round">
+              SignUp
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
