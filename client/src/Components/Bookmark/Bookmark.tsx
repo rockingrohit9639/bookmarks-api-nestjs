9f05c47d6687fa7dce2a1ac3d8faf765ee1ce381
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-24 21:15:16 +0530

    added update bookmark modal

--- client/src/Components/Bookmark/Bookmark.tsx
+++ client/src/Components/Bookmark/Bookmark.tsx
@@ -1,25 +1,74 @@
-import { Card } from 'antd';
-import React from 'react';
+import { Button, Card, Form, Input, Modal } from 'antd';
+import React, { useState } from 'react';
 import './Bookmark.css';
-import { LinkOutlined } from '@ant-design/icons';
+import { LinkOutlined, EditOutlined } from '@ant-design/icons';
 
 const Bookmark: React.FC = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const handleClose = () => setVisible(false);
+
   return (
-    <Card
-      title="Google"
-      extra={
-        <a href="http://google.com" target="_blank" rel="noreferrer">
-          <LinkOutlined />
-        </a>
-      }
-      style={{ width: 300 }}
-    >
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ducimus
-        doloremque accusamus culpa deserunt? Vero, nam! Ipsum fugit aperiam
-        porro?
-      </p>
-    </Card>
+    <>
+      <Card
+        title="Google"
+        extra={
+          <div className="flex align-items-center">
+            <a href="http://google.com" target="_blank" rel="noreferrer">
+              <LinkOutlined />
+            </a>
+            <Button type="link" onClick={() => setVisible(true)}>
+              <EditOutlined />
+            </Button>
+          </div>
+        }
+        style={{ width: 300 }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ducimus
+          doloremque accusamus culpa deserunt? Vero, nam! Ipsum fugit aperiam
+          porro?
+        </p>
+      </Card>
+      <Modal
+        title="Basic Modal"
+        visible={visible}
+        onCancel={handleClose}  
+        okButtonProps={{ hidden: true }}
+      >
+        <Form name="update-bookmark" autoComplete="off" layout="vertical">
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[{ required: true, message: 'Please input title!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Description"
+            name="description"
+            rules={[{ required: true, message: 'Please input description!' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+
+          <Form.Item
+            label="Link"
+            name="link"
+            rules={[{ required: true, message: 'Please input link!' }]}
+          >
+            <Input type="url" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Update
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 
