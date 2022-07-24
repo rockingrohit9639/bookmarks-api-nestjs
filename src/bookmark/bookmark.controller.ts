c8ab5831afcd43eddfd4de1e9fdb6625511871ff
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-24 15:30:38 +0530

    created getUserBookmarks feature

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
+import { GetUser } from 'src/decorator';
+import { JwtGuard } from 'src/guard';
+import { BookmarkService } from './bookmark.service';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(private readonly bookmarkService: BookmarkService) {}
+
+  // Get all bookmarks for a user
+  @UseGuards(JwtGuard)
+  @Get()
+  getUserBookmarks(@GetUser('id') id: number): Promise<Bookmark[]> {
+    return this.bookmarkService.getUserBookmarks(id);
+  }
+  // Create a new bookmark
+  // Update info in bookmark
+  // Delete a bookmark
+}
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUserBookmarks(userId: number): Promise<Bookmark[]> {
+    const bookmarks = await this.prisma.bookmark.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return bookmarks;
+  }
+}
